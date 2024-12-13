18df5524459d60e338afb99fc34ca74743ee5c79
phantasmavaga <phantasmavaga00@proton.me>
2024-12-13 12:34:00 +0300

    first commit

---
+++ var.py
@@ -0,0 +1,6 @@
+# değişkenler sayı , aritmetik ya da mantıksal operatörlerle başlayamaz
+# değişkenler rezerve kelimelerle başlayamaz
+# işte kabul edilebilecek bazı değişkenler
+ali1=22
+metin1=15
+keli123me="kelime"
